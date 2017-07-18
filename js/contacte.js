f7c118981274a9d682531bf77412dc853e1f95f3
dariuscrisan37 <dariuscrisan37@gmail.com>
2017-07-18 20:34:10 +0300

    remove contacts without real remove

--- js/contacte.js
+++ js/contacte.js
@@ -1,42 +1,28 @@
-function getRow (firstName, lastName, phone) {
-    if (phone == undefined) {
-        phone = '';
-    }
-    if (typeof lastName == 'undefined') {
-        lastName = '';
-    }
-    // firstName = firstName || '';
+function getRow(contact) {
+    var id = contact.id || '';
+    var firstName = contact.firstName || '';
+    var lastName = contact.lastName || '';
+    var phone = contact.phone || '';
 
-    var row = '<tr><td>' + (firstName || '') + '</td><td>' + lastName + '</td><td>' + phone + '</td></tr>';
-    return row;
+     return '<tr><td>'+lastName+'</td><td>'+firstName+'</td><td>'+phone+'</td>'+ '' +
+        '<td>[<a href="date/remove.html?id=' + id + '">x</a>]</td>'+'</tr>';
 }
+
 var contacte = [];
-//iterez contacte si fac chestii
-contacte.forEach(function(contact){
-    console.info('calling ...'+contact.firstName);
-});
 
-//ajax call
-$.ajax('servlet/contacte.json').done(function (totJsonul) {
-    //console.info("raspuns", contacteleDinJon);
-    totJsonul.forEach(function(contact){
-        console.info('feed ...'+contact.firstName);
-        createRow(contact);
-    });
+var tableContent = '';
+
+// for(var i = 0; i < contacte.length; i++) {
+//     createRow(contacte[i])
+// }
+
+function createRow(contact){
+    tableContent += getRow(contact);
+}
+
+$.ajax('date/contacte.json').done(function(contacte){
+    console.info('contacte', contacte);
+    contacte.forEach(createRow);
     $("#contacts-list tbody").html(tableContent);
+
 });
-
-console.info('this happens before ajax finishes the task');
-
-
-// contacte.forEach(createRow);
-
-// $("#contacts-list tbody") [0] .innerHTML = row;
-
-
-// #contacts-list tbody {color: red}
-
-// 1. convert from array from arrays into json
-// 2. load contacts form json file with ajax
-// 3. remove contacts (UI)
-// 4. edit contacts (UI)
