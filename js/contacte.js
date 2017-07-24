a2dae813de43ce802ae093d9d41b58e58ca1c27a
dariuscrisan37 <dariuscrisan37@gmail.com>
2017-07-25 01:31:03 +0300

    remodel site and projects

--- js/contacte.js
+++ js/contacte.js
@@ -4,12 +4,12 @@ function getRow(contact) {
     var lastName = contact.lastName || '';
     var phone = contact.phone || '';
 
-     return '<tr><td>'+lastName+'</td><td>'+firstName+'</td><td>'+phone+'</td>'+ '' +
+    return '<tr><td>' + lastName + '</td><td>' + firstName + '</td><td>' + phone + '</td>' +
         '<td class="actions">'+
-         '<span><a href="date/remove.html?id=' + id + '">x</a></span> '+
-         '<span><a class="edit" href="#">edit</a></span>'
-    '</td>'+
-    '</tr>';
+        '<span><a href="date/remove.html?id=' + id + '">&#x2716;</a></span> '+
+        '<span><a href="#">&#x270E;</a></span>'+
+        '</td>' +
+        '</tr>';
 }
 
 var contacte = [];
@@ -30,15 +30,3 @@ $.ajax('date/contacte.json').done(function(contacte){
     $("#contacts-list tbody").html(tableContent);
 
 });
-
-// $('.edit').click(function (){
-//     editContact('Matei','Nicolae','151515');
-// });
-
-// function editContact(firstName, lastName, phone) {
-//     $('input[name=firstName]'.val(firstName);
-//     $('input[name=firstName]'.val(lastName);
-//     $('input[name=firstName]'.val(phone);
-// }
-
-
--- js/semafor.js
+++ js/semafor.js
@@ -3,22 +3,22 @@ document.getElementById('slowButton').onclick = illuminateYellow;
 document.getElementById('goButton').onclick = illuminateGreen;
 
 function illuminateRed() {
-  clearLights();
-  document.getElementById('stopLight').style.backgroundColor = "red";
+    clearLights();
+    document.getElementById('stopLight').style.backgroundColor = "red";
 }
 
 function illuminateYellow() {
-  clearLights();
-  document.getElementById('slowLight').style.backgroundColor = "yellow";
+    clearLights();
+    document.getElementById('slowLight').style.backgroundColor = "yellow";
 }
 
 function illuminateGreen() {
-  clearLights();
-  document.getElementById('goLight').style.backgroundColor = "green";
+    clearLights();
+    document.getElementById('goLight').style.backgroundColor = "green";
 }
 
 function clearLights() {
-  document.getElementById('stopLight').style.backgroundColor = "black";
-  document.getElementById('slowLight').style.backgroundColor = "black";
+    document.getElementById('stopLight').style.backgroundColor = "black";
+    document.getElementById('slowLight').style.backgroundColor = "black";
     document.getElementById('goLight').style.backgroundColor = "black";
 }
