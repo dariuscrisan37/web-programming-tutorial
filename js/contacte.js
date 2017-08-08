69992f60127c71cae44776455c66223bf75120c6
Alex <simiendo.as@gmail.com>
2017-08-08 11:09:43 +0300

    lab 10 ajax

--- js/contacte.js
+++ js/contacte.js
@@ -5,7 +5,11 @@ function getRow(contact) {
     var phone = contact.phone || '';
 
     return '<tr><td>'+lastName+'</td><td>'+firstName+'</td><td>'+phone+'</td>'+ '' +
-        '<td class="actions">[<a href="date/remove.html?id=' + id + '">x</a>]</td>'+'</tr> <span>a href="#"</span>';
+        '<td class="actions">' +
+        '<span><a href="date/remove.html?id=' + id + '">&#x2716</a></span>'+
+        '<span><a href="#">&#x270E</a></span>'+
+        '</td>' +
+        '</tr>0';
 }
 
 var contacte = [];
@@ -25,4 +29,13 @@ $.ajax('date/contacte.json').done(function(contacte){
     contacte.forEach(createRow);
     $("#contacts-list tbody").html(tableContent);
 
+    $(.edit).click(function (){
+        editContact('Matei', 'Nicolae', '0232');
+    });
 });
+
+function editContact(firstName, lastName, phone) {
+    $('input[name=firstName]').val(firstName);
+    $('input[name=lastName]').val(lastName);
+    $('input[name=phone]').val(phone);
+}
