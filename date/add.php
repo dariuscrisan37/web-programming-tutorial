354551abbe3ca2daac22c52039079f004f22f58d
Alex <simiendo.as@gmail.com>
2017-08-29 10:10:27 +0300

    lab 13 baze de date

--- date/add.php
+++ date/add.php
@@ -1,40 +1,52 @@
 <html>
 <head>
-    <meta http-equiv="refresh" content="0;url=../contacte.html" />
+    <meta http-equiv="refresh" content="0;url='../contacte.html'" />
 </head>
 <body>
+<div id="contact-id">
+
+</div>
 
 <?php
-$contentString = file_get_contents("contacte.json");
-$contacte = json_decode($contentString, true);
 
-function getNextId(){
-    $idString = file_get_contents("last.contact.id");
+function getNextId() {                                   // de verificat
+    $idString = file_get_contents("last.contact.id.json");
     $id = intval($idString);
     $id++;
-    file_put_contents("last.contact.id", $id);
+    file_put_contents("last.contact.id.json", $id);
     return $id;
 }
 
-//$last_item    = end($contacte);
-//$last_contact_id = $last_item['id'];
+$contentString = file_get_contents("contacte.json");    //citire contacte.json ca string
+$contacte = json_decode($contentString, true);
 
-$id = getNextId ();
+$id = getNextId();
 
 $newPerson = array(
-    'id' => $id,
-    "firstName" => $_GET["firstName"],
-    "lastName" => $_GET["lastName"],
-    "phone" => $_GET["phone"],
+    "id" => $id,
+    "firstName" => $_GET['firstName'],    // citire paramentru form
+    "lastName" => $_GET['lastName'],
+    "phone" => $_GET['phone']
 );
 
-$contacte[] = $newPerson;  //add new person into array
-
-$contentString = json_encode($contacte);
+$contacte[] = $newPerson;    // add new person into array
+$contentString = json_encode($contacte, true);    // salvare inapoi in fisier
 file_put_contents("contacte.json", $contentString);
+
+header('Location: ../contacte.html');
 
 ?>
 
-contact added...
+contact added
+
+
+<!--    <script>-->
+<!--    var parameters = location.search.substr(1);-->
+<!--    var paramArray = parameters.split('&');-->
+<!--    console.info(paramArray);-->
+<!--        -->
+<!--    document.getElementById('contact-id').innerHTML = paramArray.join('; <br>') + ';';-->
+<!--    </script>-->
 </body>
 </html>
+
